cf080a320a43813061b2625847818e86eea81d99
Cansu Dogan <cansuiris@gmail.com>
2026-05-12 22:53:42 +0200

    pause and pause menu implemented

--- Scenes/PauseMenu/PauseMenu.cs
+++ Scenes/PauseMenu/PauseMenu.cs
@@ -1,12 +1,16 @@
 using Godot;
 using System;
 
-public partial class Hospital : Node2D
+public partial class PauseMenu : Control
 {
+
+	[Export] private TextureButton _resumeButton;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		SignalHub.Instance.OnMenuPressed += OnMenuPressed;
+		_resumeButton.Pressed += OnResumePressed;
 	}
 
     public override void _ExitTree()
@@ -15,19 +19,20 @@ public partial class Hospital : Node2D
     }
 
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (@event.IsActionPressed("menu"))
-		{
-			SignalHub.EmitOnMenuPressed();
-		}
-    }
-
 
     private void OnMenuPressed()
     {
-        GD.Print("Menu pressed");
+        GetTree().Paused = true;
+		Show();
     }
+
+	private void OnResumePressed()
+	{
+		Hide();
+		GetTree().Paused = false;
+	}
+
+
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
