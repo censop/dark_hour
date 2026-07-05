70b6bb65d079b90e10da4b7dc96a632049da3928
Cansu Dogan <cansuiris@gmail.com>
2026-07-05 14:35:12 +0200

    LockedDoor script initiated

--- Scenes/Props/Doors/LockedDoor.cs
+++ Scenes/Props/Doors/LockedDoor.cs
@@ -3,13 +3,33 @@ using System;
 
 public partial class LockedDoor : StaticBody2D
 {
+	[Export] Area2D _interactionArea;
+	[Export] String DoorId = "Door_001";
+
+	private bool _isBodyInsideArea = false;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_interactionArea.BodyEntered += OnBodyEntered;
+		_interactionArea.BodyExited += OnBodyExited;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    private void OnBodyExited(Node2D body)
+    {
+        if (body is Player) _isBodyInsideArea = false;
+		GD.Print("Cant intract with door");
+    }
+
+
+    private void OnBodyEntered(Node2D body)
+    {
+        if (body is Player) _isBodyInsideArea = true;
+		GD.Print("Can intract with door");
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 	}
 }
