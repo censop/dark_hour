62cebfe5fb573f6cf24c7cd53409684be0b27a92
Cansu Dogan <cansuiris@gmail.com>
2026-07-17 13:42:19 +0300

    y-sorting implemented

--- Globals/ScenePaths.cs
+++ Globals/ScenePaths.cs
@@ -4,7 +4,8 @@ using System;
 public partial class ScenePaths : Node
 {
     public static ScenePaths Instance {private set; get;}
-    public static String HospitalPath = "res://Scenes/Hospital/Hospital.tscn";
+    public static String HospitalPath = "res://Scenes/Map/Hospital/Hospital.tscn";
+    
     public override void _Ready()
     {
         Instance = this;
