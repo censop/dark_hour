bf2b0230d6580ab5ee00ecb140fe0d08322b5ac5
Cansu Dogan <cansuiris@gmail.com>
2026-08-13 11:18:58 +0300

    all ui altered

--- Scenes/UI/PauseMenu/PauseMenu.cs
+++ Scenes/UI/PauseMenu/PauseMenu.cs
@@ -5,7 +5,6 @@ public partial class PauseMenu : Control
 {
 
 	[Export] private TextureButton _resumeButton;
-	[Export] private TextureButton _saveButton;
  
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -13,20 +12,12 @@ public partial class PauseMenu : Control
 		Hide();
 		SignalHub.Instance.OnMenuPressed += OnMenuPressed;
 		_resumeButton.Pressed += OnResumePressed;
-		_saveButton.Pressed += OnSavePressed;
 	}
 
 	public override void _ExitTree()
     {
         SignalHub.Instance.OnMenuPressed -= OnMenuPressed;
     }
-
-    private void OnSavePressed()
-    {
-		AudioManager.Instance.PlayUIClickSound();
-        SignalHub.EmitOnSaveGame();
-    }
-
     private void OnMenuPressed()
     {
 		AudioManager.Instance.PlayUIClickSound();
