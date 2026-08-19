98952833175d59b0bced467cd7b10dd7abb6e034
Cansu Dogan <cansuiris@gmail.com>
2026-08-19 14:56:19 +0300

    NoteUI completely implemented

--- Scenes/UI/NoteUI/NoteUi.cs
+++ Scenes/UI/NoteUI/NoteUi.cs
@@ -4,11 +4,22 @@ using System;
 public partial class NoteUi : Control
 {
 	[Export] Label _note;
+
+    [Export] TextureButton _exitButton;
 	public override void _Ready()
 	{
 		Hide();
 		SignalHub.Instance.OnNoteInteracted += OnNoteInteracted;
+        _exitButton.Pressed += ExitButtonPressed;
 	}
+
+    private void ExitButtonPressed()
+    {
+        AudioManager.Instance.PlayUIClickSound();
+		Hide();
+		GetTree().Paused = false;
+    }
+
 
     public override void _ExitTree()
     {
@@ -22,13 +33,19 @@ public partial class NoteUi : Control
 		{
 
             _note.Text = specificNote.Body;
-			Show();
+			FreezeAndShow();
 			
 		}
         else
         {
             GD.PrintErr($"Could not find a note with ID: {noteCode} in the JSON file.");
         }
+    }
+
+    private void FreezeAndShow()
+    {
+        GetTree().Paused = true;
+		Show();
     }
 
 }
